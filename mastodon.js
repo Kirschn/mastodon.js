232bf7cf02666a15c040e8087f8611bdf0902827
新都心(Neet Shin) <nucx@dio-vox.com>
2017-06-03 15:24:35 +0530

    Added getArray()

--- mastodon.js
+++ mastodon.js
@@ -65,6 +65,58 @@ var MastodonAPI = function(config) {
                 }
             });
         },
+        getArray: function (endpoint) {
+            // for GET API calls
+            // can be called with two or three parameters
+            // endpoint, callback
+            // or
+            // endpoint, queryData, callback
+            // where querydata is an object {["paramname1", "paramvalue1], ["paramname2","paramvalue2"]}
+
+            // variables
+            var queryData, callback,
+                queryStringAppend = "?";
+
+            // check with which arguments we're supplied
+            if (typeof arguments[1] === "function") {
+                queryData = {};
+                callback = arguments[1];
+            } else {
+                queryData = arguments[1];
+                callback = arguments[2];
+            }
+            // build queryData Object into a URL Query String
+            for (var i in queryData) {
+                if (queryData.hasOwnProperty(i)) {
+                    if (typeof queryData[i] === "string") {
+                        queryStringAppend += queryData[i] + "&";
+                    } else if (typeof queryData[i] === "object") {
+                        for ( var j in queryData[i].data ){
+                          queryStringAppend += queryData[i].name + "[]="+ queryData[i].data[j] + "&";
+                        }
+                    }
+                }
+            }
+            // ajax function
+            $.ajax({
+                url: apiBase + endpoint + queryStringAppend,
+                type: "GET",
+                headers: {"Authorization": "Bearer " + config.api_user_token},
+                success: function(data, textStatus, xhr) {
+
+                    //weeey it was successful
+                    console.log("Successful GET API request to " +apiBase+endpoint);
+                    responce_headers = xhr.getAllResponseHeaders();
+
+                    //aaand start the callback
+                    //might have to check what "textStatus" actually is, jquery docs are a bit dodgy
+                    callback(data,textStatus);
+                },
+                error: function(xhr, textStatus, errorThrown) {
+                  putMessage(`[${xhr.status}] ${xhr.responseJSON['error']}`);
+                }
+            });
+        },
         getOther: function (domainAndEndpoint) {
 
             var queryData, callback,
